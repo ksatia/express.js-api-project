ca3793f6f19a7235adca4860cc159aca4f85908b
ksatia <ksatia@gmail.com>
2017-04-26 14:41:21 +0000

    change jade file to highlight that its for testing purposes

--- server.js
+++ server.js
@@ -11,24 +11,36 @@ app.get('/:date', function(req, res){
     //res.render("index");
 
     //res.send() assumes the content-type of the response to be html and then calls end whereas res.end() makes no such assumption
-    res.send(routingResponse(req));
+    res.json(routingResponse(req));
 }).listen(8080)
 
 
 
 function routingResponse(req) {
     var dateFormatData = req.params.date;
-    
+    //we're looking for a unix stamp or a date in the format of mm+dd+yyyy
     //log request url parameters just to see the properties
-    console.log(url.parse(req.url));
+    //console.log(url.parse(req.url));
     
-    if (typeof dateFormatData === "string") {
-        var unix = "1234567";
-        var naturalLanguageDate = dateFormatData;
+    var dateFormatOptions = {
+        year : "numeric",
+        month: "long",
+        day: "2-digit"
     }
+    
+    //check for a natural language date passed in as the url query
+    if (isNaN(dateFormatData)) {
+        var dateObject = new Date(dateFormatData);
+        
+        var unix = dateObject.getTime()/1000;
+        //format naturallanguage to local date using formattion options
+        var naturalLanguageDate = dateObject.toLocaleDateString("en-us", dateFormatOptions);
+    }
+   
+    //program control falls here if the url query is a unix number
     else {
-        var unix = "1234567";
-        var naturalLanguageDate = "poopeedoopee";
+        var unix = dateFormatData;
+        var naturalLanguageDate = new Date(dateFormatData*1000).toLocaleString("en-us", dateFormatOptions);
     }
     
     var responseData = {unix: unix, naturalDate: naturalLanguageDate};
